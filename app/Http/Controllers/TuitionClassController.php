26dbb8dba8b53f1f5acb360433b69b7dd5c01fc9
Kahahu <edward.kahahu@strathmore.edu>
2024-06-27 15:12:23 +0300

    commit1

---
+++ app/Http/Controllers/TuitionClassController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TuitionClassController extends Controller
+{
+    //
+}
---
+++ app/Models/TuitionClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TuitionClass extends Model
+{
+    use HasFactory;
+}
